9405a244aed4bc1704dd170bf13551ddb2930888
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2021-08-11 13:18:30 +0300

    Start

---
+++ index.js
@@ -0,0 +1,11 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+const btnStart = document.querySelector('[data-action="start"]')
+const btnStop = document.querySelector('[data-action="stop"]')
+
