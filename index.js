41c6abea62acddfea688c62a0f730e02eede8910
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2021-08-11 14:19:31 +0300

    switcher update

--- index.js
+++ index.js
@@ -6,6 +6,35 @@ const colors = [
     '#009688',
     '#795548',
 ];
+const background = document.querySelector('body')
 const btnStart = document.querySelector('[data-action="start"]')
 const btnStop = document.querySelector('[data-action="stop"]')
 
+
+background.style.backgroundColor = '#FFFFFF'
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function colorPicker() {
+    const color = colors[randomIntegerFromInterval(0, colors.length)];
+    return color
+}
+
+function colorChanger() {
+    background.style.backgroundColor = `${colorPicker()}`
+}
+
+function onBtnStartClick() {
+    intervalId = setInterval(colorChanger, 1000)
+    btnStart.setAttribute("disabled", "disabled");
+}
+
+function onBtnStopClick() {
+    clearInterval(intervalId)
+    btnStart.removeAttribute("disabled", "disabled");
+}
+
+btnStart.addEventListener('click', onBtnStartClick)
+btnStop.addEventListener('click', onBtnStopClick)
